5a66c606aa386176d1303fef4bef8131673bde6f
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-15 09:17:24 +0200

    Adding title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
 
+import streamlit as st
+
+st.set_page_config(page_title="Bicimad demand prediction", layout="wide")
+st.header("Demand prediction BICIMAD")
+
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("Evolution of the demand")
+
+                   
