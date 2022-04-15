a658944b95a70f843eb22837547455cc797a1527
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-15 21:46:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,13 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("Evolution of the demand 1")
+        st.header("Evolution of the demand")
+        
+    with right_column:
         df = pd.read_csv("stations_final.csv")
         df['lat']= df['latitude']
         df['lon']=df['longitude']
         df = df[['lat', 'lon']]
         st.map(df)
+     
 
