371726ca6629a46048085e4974d8fccaf34ff38b
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-15 21:57:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ with st.container():
     left_column, right_column = st.columns(2)
     with left_column:
         st.header("Evolution of the demand")
+        sel_col, disp_col = st.beta_columns(2)
+        selector = sel_col.selectbox("Zone", options = [100,200])
         
     with right_column:
         df = pd.read_csv("stations_final.csv")
