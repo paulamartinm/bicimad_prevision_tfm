a672dbd9dc159b0f0ff4064d2b2c063e5baeac58
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-15 22:02:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+df = pd.read_csv("stations_final.csv")
 st.set_page_config(page_title="Bicimad demand prediction", layout="wide")
 st.header("Demand prediction BICIMAD")
 
@@ -13,11 +13,12 @@ with st.container():
     left_column, right_column = st.columns(2)
     with left_column:
         st.header("Evolution of the demand")
+        
     
-        option = st.selectbox('Zone', (100,200))
+        option = st.selectbox('Zone', list(df['postal_code'].unique())
         
     with right_column:
-        df = pd.read_csv("stations_final.csv")
+       
         df['lat']= df['latitude']
         df['lon']=df['longitude']
         df = df[['lat', 'lon']]
