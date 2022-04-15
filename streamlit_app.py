e7d293cb05d43dc5f50129d957eba3896672e80c
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-15 22:05:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,13 @@ with st.container():
     with left_column:
         st.header("Evolution of the demand")
         
-        left_column_1, medium_column_1, right_column_1 = st.columns(3)
+        left_column_1, right_column_1 = st.columns(2)
         
         with left_column_1:
             option = st.selectbox('Postal code', list(df['postal_code'].unique()))
         
+        with right_column_1:
+            
     with right_column:
        
         df['lat']= df['latitude']
