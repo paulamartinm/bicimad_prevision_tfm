01c9ac93bcb4fca9167cb89068b5b128dcfec41f
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-04-16 17:42:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,49 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-df = pd.read_csv("stations_final.csv")
-st.set_page_config(page_title="Bicimad demand prediction", layout="wide")
-st.header("Demand prediction BICIMAD")
+from streamlit_option_menu import option_menu
 
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("Evolution of the demand")
+plt.style.use("ggplot")
+
+#Navigation menu
+with st.sidebar:
+    selected = option_menu(
+        menu_title = "Main menu",
+        options= ["Prediction of demand", "Dashboard"],
+    )
+
+df_stations = pd.read_csv("stations_final.csv")
+
+if selected == "Prediction of demand":
+    
+    st.set_page_config(page_title="Bicimad demand prediction", layout="wide")
+    st.header("Demand prediction BICIMAD")
+
+    st.sidebar.
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.header("Evolution of the demand")
         
         
-        postal_code = st.selectbox('Postal code', list(df['postal_code'].unique()))
+            postal_code = st.selectbox('Postal code', list(df_stations['postal_code'].unique()))
      
             
-    with right_column:
+        with right_column:
        
-        df['lat']= df['latitude']
-        df['lon']=df['longitude']
-        df = df[['lat', 'lon', 'postal_code']]
-        df = df[df['postal_code']==postal_code]
-        st.map(df)
+            df_stations['lat']= df_stations['latitude']
+            df_stations['lon']=df_stations['longitude']
+            df_stations = df_stations[['lat', 'lon', 'postal_code']]
+            df_stations = df_stations[df_stations['postal_code']==postal_code]
+            st.map(df)
+          
+        
+        
+if selected == "Dashboard":
+    st.set_page_config(page_title="Dashboard of evolution", layout="wide")
+
+    
      
 
 
