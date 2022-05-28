873701d9c3fe8cf30337bfdd937a6b44f0841400
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-05-28 15:04:47 +0200

    Update streamlit_app.py

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -142,6 +142,8 @@ if selected == "Dashboard":
 
   with st.container():
      st.subheader('Evolution of demand in the selected year')
+     rides_per_hour['datetime'] = pd.to_datetime(rides_per_hour['datetime'])
+     rides_per_hour.sort_values(by='datetime', inplace=True)
      fig = px.line(rides_per_hour[rides_per_hour_months['year']==year], x= 'datetime', y='rides')
      fig.update_layout(
                 showlegend = True,
