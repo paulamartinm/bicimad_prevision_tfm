78dfdae79ce014d60cc648f4d166172b88a4de62
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-06-11 14:22:27 +0200

    Update streamlit_app.py

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -135,7 +135,7 @@ if selected == "Prediction of demand":
                  fig = go.Figure(data=go.Table(header=dict(values=list(evaluation_models[['model', 'MSE', 'MAE', 'RMSE', 'R2']].columns), fill_color='FD8E72',
                                                                                           align = left),
                                                cells = disct(values=[evaluation_models.model, evaluation_models.MSE, evaluation_models.MAE, evaluation_models.RMSE,
-                                                                     evaluation_models.R2], fill_color = 'E5ECF6, align = 'left')))
+                                                                     evaluation_models.R2], fill_color = 'E5ECF6', align = 'left')))
                  st.write(fig)                                                                         
             else:
                 st.metric(label="Maximum demand", value = int(max_demand))
