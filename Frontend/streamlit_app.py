d4dffe747295536df9a8ea5c92ac781a9cc0ce83
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-06-14 23:38:52 +0200

    Update streamlit_app.py

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -250,8 +250,8 @@ if selected == "Prediction of demand":
                           value=stations_postal_code['id'].count())
                 # col1, col2 =  st.columns(2)
                 # st.metric(label="Maximum demand hour", value = peak_hour_max_demand)
-                st.metric(label="Day of maximum demand",
-                          value=str(peak_day_max_demand)[7:27])
+                #st.metric(label="Day of maximum demand",
+                          #value=str(peak_day_max_demand)[7:27])
 
                 st.metric(label="Total docks",
                           value=stations_postal_code['total_bases'].sum())
