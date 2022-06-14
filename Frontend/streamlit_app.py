b3781f138f903a46f8d00cbb024be29bfb61ca3e
paulamartinm <92890363+paulamartinm@users.noreply.github.com>
2022-06-14 23:33:43 +0200

    Update streamlit_app.py

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -207,9 +207,10 @@ if selected == "Prediction of demand":
         with middle_column:
                 st.subheader("Stations list")
                 if visualization == "Demand per zones":
-                     st.dataframe(data = df_stations[df_stations['postal_code'] == postal_code]) 
+                     df = df_stations[df_stations['postal_code']==postal_code]
+                     st.dataframe(data = df[['name', 'address']]) 
                 elif visualization == "All stations":
-                     st.dataframe(data = df_stations)
+                     st.dataframe(data = df_stations[['name', 'address', 'postal_code']])
 
         # LEFT SIDE VISUALIZATION
         with left_column:
